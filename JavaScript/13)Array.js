6040a6e19a346e125474ec0cd61de81f349e422f
Jan Peterek <peterekja@seznam.cz>
2023-01-04 13:42:33 +0100

    no message

--- JavaScript/13)Array.js
+++ JavaScript/13)Array.js
@@ -13,4 +13,40 @@ console.log(age[2]);
 
 var david = ["David", "Setek", 33, 1980, true]
 
-/***********************************************/
+/*********************2x empty v array a jejich vymena**************************/
+var employees = ["David", "Jane", "Diana"];
+employees[0] = "Martin";
+employees[1] = "Anna";
+employees[5] = "Eduard";
+
+console.log(employees)
+
+/*********************************************/
+var employees = ["David", "Jane", "Diana"];
+
+employees.push("John");  //push Joshn do konce array
+employees.push("John2");  
+employees.unshift("Albert"); // pridani Alberta na 0 pozici v array
+
+employees.pop(); // odebrani posledniho z Array
+employees.shift(); // odebrani prvniho z Array, a prepise to index
+
+
+console.log(employees)
+
+/***********************************/
+
+var employees = ["David", "Jane", "Diana", "Martina", "Eduard"];
+
+    console.log(employees.indexOf("David"));   //vypis indexu do console, ale jen prvni
+    console.log(employees.indexOf("Eduard"));
+    console.log(employees.indexOf("Natalie")); //kdyz nenajde v poli dany prvek, vypise -1
+
+    console.log(employees.length);  //vypis poctu hodnot v Array
+
+    console.log(employees[employees.length-1]); // vypis posledniho clena v Array
+
+    
+/**********************************************/
+
+
---
+++ JavaScript/14)Objects.js
@@ -0,0 +1,106 @@
+var employees = ["David", "Jane", "Diana"];
+
+
+var david = {
+    firstName: "David",
+    lastName: "Setek",
+    birthYear: 1980,
+    driverLicence: true,
+    family: ["John", "Anna", "Diana"]
+}
+
+
+console.log(david);
+console.log(david.lastName);
+
+console.log(david["firstName"]); //jiny zapis
+
+/******************************************/
+//druha moznost jak zapsat object
+
+var diana = new Object();
+diana.firstName = "Diana";
+diana.lastName = "Novotna";
+diana.birthYear = 1999;
+diana.driveLicence = false;
+
+console.log(diana);
+
+diana.firstName = "Ales"; // zmena hodnoty
+
+
+
+/******************METODY u objektu = funkce************************/
+
+
+var employees = ["David", "Jane", "Diana"];
+
+
+var david = {
+    firstName: "David",
+    lastName: "Setek",
+    birthYear: 1980,
+    driverLicence: true,
+    family: ["John", "Anna", "Diana"],
+    calculateAge: function(){
+        return 2020 - david.birthYear;
+    }
+}
+
+
+
+var diana = {
+    firstName: "Diana",
+    lastName: "Setek",
+    birthYear: 2000,
+    driverLicence: true,
+    family: ["John", "Anna", "Diana"],
+    calculateAge: function(){
+        return 2020 - this.birthYear;  //this odkazuje na var diana
+    }
+}
+
+var eduard = {
+    firstName: "Eduard",
+    lastName: "Setek",
+    birthYear: 1990,
+    driverLicence: true,
+    family: ["John", "Anna", "Diana"],
+    calculateAge: function(){
+        return 2020 - this.birthYear;    //this odkazuje na var eduard
+    }
+}
+
+var anna = {
+    firstName: "Anna",
+    lastName: "Setek",
+    birthYear: 1950,
+    driverLicence: true,
+    family: ["John", "Anna", "Diana"],
+    calculateAge: function(){
+        return 2020 - this.birthYear;    //this odkazuje na var anna
+    }
+}
+
+console.log(david.calculateAge());
+console.log(diana.calculateAge());
+console.log(eduard.calculateAge());
+console.log(anna.calculateAge());
+
+
+//*******************po uprave, kdy je prirazen age do array********/
+var diana = {
+    firstName: "Diana",
+    lastName: "Setek",
+    birthYear: 2000,
+    driverLicence: true,
+    family: ["John", "Anna", "Diana"],
+    calculateAge: function(){
+        this.age = 2020 - this.birthYear;  //this.age najednou odkazuje a prizadi do objektu
+    }
+}
+diana.calculateAge();  // volani funkce
+console.log(diana);
+
+
+
