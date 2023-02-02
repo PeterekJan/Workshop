7e366550baedc2d99069276c9d130fedf45afd27
Jan Peterek <peterekja@seznam.cz>
2023-02-02 15:13:24 +0100

    no message

---
+++ 8-js/index.js
@@ -0,0 +1,24 @@
+
+// Define function
+function greetings(name) {
+    console.log(name +": Hello there!");
+}
+
+var a = 2;
+var square = a * a;
+
+
+function content(square){
+    if(a === 0){
+        console.log("nulou nepodelis");
+    }else if(a > 0){
+        console.log(`Content square is${square}`);
+    }
+}
+content( `${" " + square + "[x^2]"}`)
+
+
+// Call function right when cli command is called
+//greetings("General Kenobi");
+
+
---
+++ JavaScript/24)PaddedStart.js
@@ -0,0 +1,9 @@
+var text = '5';
+var padded = text.padEnd(8,"0");
+
+console.log(padded)
+
+var text = '5';
+var padded2 = text.padStart(20,"XXXS");
+
+console.log(padded2)
