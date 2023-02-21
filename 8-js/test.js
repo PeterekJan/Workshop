85611fedd52d15479decbb18ed313908df1ee03b
Jan Peterek <peterekja@seznam.cz>
2023-02-21 13:48:09 +0100

    no message

--- 8-js/test.js
+++ 8-js/test.js
@@ -1,28 +1,29 @@
-var newArray = [3,10,0.1,0.1,15,88,101,88,72,88,-5];
+var newArray = [3,10,0.1,0.1,15,88,101,88,72,88,-5,];
 
-    newObject = {
-        count: newArray.length,
-        min: 0,
-        max: 0,
-        total:0,
-        avg: 0,
-        modus:[],
-        median:0
-       
-    };
-    function minNum(number){
-        var min = 0;
-        for(var i = 0;i < newArray.length; i++){
-            var element = number[i];
-            if(element < min){
-                min = number[i]
-            };
+newObject = {
+    count: newArray.length,
+    min: 0,
+    max: 0,
+    total:0,
+    avg: 0,
+    modus:[],
+    median:0
+    
+};
+
+function minNum(number){
+    var min = 0;
+    for(var i = 0;i < number.length; i++){
+        var element = number[i];
+        if(element < min){
+            min = number[i]
         };
+    };
         //console.log(number[1])
         //console.log(element)
-        return min;
+    return min;
         
-    };
+};
 
 var result = minNum(newArray)
 //console.log(result)
@@ -30,7 +31,7 @@ newObject.min = result
 
 function maxNum(number){
     var max = 0;
-    for(var i = 0;i < newArray.length; i++){
+    for(var i = 0;i < number.length; i++){
         var element = number[i];
         if(element > max){
             max = number[i];
@@ -50,19 +51,36 @@ newObject.max = result2;
 //var result3 = Math.max.apply(null,newArray);
 //console.log(result3);
 
-function summary(newArray){
+function sum(number){
     var total = 0;
-    for(var i = 0;i < newArray.length; i++){
-        
-    }
-}
+    for(var i = 0; i < number.length; i++){
+        total += number[i]
+
+    };
+    //console.log(total)
+    //console.log(number[1])
+    return total
+};
+
+var result4 = sum(newArray);
+newObject.total = result4
+//console.log(sum(newArray))
+    
+function avg(number){
+    var total2 = 0;
+    for(var i = 0;i < number.length; i++ ){
+        total2 += number[i]
+    };
+
+    return total2
 
     
-   
+}  
 
 
 
-console.log(newObject)
+
+//console.log(newObject)
 
 
 
