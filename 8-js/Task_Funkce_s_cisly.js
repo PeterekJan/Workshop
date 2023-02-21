14638acbb77e8d85149e0ffcc8b1a2f4aafa0ac2
Jan Peterek <peterekja@seznam.cz>
2023-02-21 13:48:45 +0100

    no message

--- 8-js/Task_Funkce_s_cisly.js
+++ 8-js/Task_Funkce_s_cisly.js
@@ -105,39 +105,83 @@ calculateGeometry(20,1)
 //        - ``median`` - medián - střední hodnota seřazené posloupnosti
 console.log("-------------CalculateGeometry-------------------")
 
-var newArray = realNum([3,10,0.1,0.1,15,88,101,88,72,88,-5]);
+var newArray = [3,10,0.1,0.1,15,88,101,88,72,88,-5,];
 
-function realNum(newArray){
+newObject = {
+    count: newArray.length,
+    min: 0,
+    max: 0,
+    total:0,
+    avg: 0,
+    modus:[],
+    median:0
+    
+};
 
-    newObject = {
-        count: newArray.length,
-        min: 0,
-        max: 0,
-        total:0,
-        avg: 0,
-        modus:[],
-        median:0
-       
+function minNum(number){
+    var min = 0;
+    for(var i = 0;i < number.length; i++){
+        var element = number[i];
+        if(element < min){
+            min = number[i]
+        };
     };
-    function minNum(number){
-        var min = 0;
-        for(var i = 0;i < newArray.length; i++){
-            var element = number[i];
-            if(element < min){
-                min = number[i]
-            }
-        }
-        return min;
-      
+        //console.log(number[1])
+        //console.log(element)
+    return min;
+        
 };
 
 var result = minNum(newArray)
+//console.log(result)
 newObject.min = result
-    //console.log(result)
-    console.log(element)
 
-
+function maxNum(number){
+    var max = 0;
+    for(var i = 0;i < number.length; i++){
+        var element = number[i];
+        if(element > max){
+            max = number[i];
+        };
+    };
+    //console.log(number[5]);
+    //console.log(element);
+    return max;
+    
 };
-//console.log(newObject)
+
+var result2 = maxNum(newArray);
+//console.log(result2);
+newObject.max = result2;
+
+
+//var result3 = Math.max.apply(null,newArray);
+//console.log(result3);
+
+function sum(number){
+    var total = 0;
+    for(var i = 0; i < number.length; i++){
+        total += number[i]
+
+    };
+    //console.log(total)
+    //console.log(number[1])
+    return total
+};
+
+var result4 = sum(newArray);
+newObject.total = result4
+//console.log(sum(newArray))
+    
+function avg(number){
+    var total2 = 0;
+    for(var i = 0;i < number.length; i++ ){
+        total2 += number[i]
+    };
+
+    return total2
+
+    
+}  
 
 
