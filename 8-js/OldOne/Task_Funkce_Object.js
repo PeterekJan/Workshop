96a42fff923716912e814cf9db12f70266c0109c
Jan Peterek <peterekja@seznam.cz>
2023-03-02 12:44:59 +0100

    no message

---
+++ 8-js/OldOne/Task_Funkce_Object.js
@@ -0,0 +1,29 @@
+//h) Objekty
+
+//1. Nastudujte si co jsou to třídy (objekty) a jak se v JS zapisují - vytvořte třídu ``Car``
+
+
+
+
+
+
+
+
+
+
+//2. Tato třída bude mít 3 atributy, které bude potřeba zadat během inicializace objektu.
+//    - ``brand``
+//    - ``model``
+//    - ``year``
+
+
+
+
+
+
+
+
+
+
+
+//3. Třída bude mít funkci s názvem ``output``, jejíž návratová hodnota bude "``brand``, ``model`` - rok výroby: ``year``"
