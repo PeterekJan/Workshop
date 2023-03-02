0dd93ae6d68a357e7801c808e95ea55af815d32b
Jan Peterek <peterekja@seznam.cz>
2023-03-02 17:29:20 +0100

    no message

--- 8-js/OldOne/Task_Funkce_Object.js
+++ 8-js/OldOne/Task_Funkce_Object.js
@@ -1,21 +1,21 @@
 //h) Objekty
 
 //1. Nastudujte si co jsou to třídy (objekty) a jak se v JS zapisují - vytvořte třídu ``Car``
-
-
-
-
-
-
-
-
-
-
 //2. Tato třída bude mít 3 atributy, které bude potřeba zadat během inicializace objektu.
 //    - ``brand``
 //    - ``model``
 //    - ``year``
+class Car{
+    constructor(brand,model,year){
+        this.brand = brand,
+        this.model = model,
+        this.year = year
+    };
 
+    output(brand,model,year){
+        return brand, model - year
+    }
+};
 
 
 
---
+++ JavaScript/Classes.js
@@ -0,0 +1,22 @@
+class Chair {
+    constructor(color, seatHeight, recliningAngle, backSupport, headSupport, padding, armRests, seatSize, isHeightAdjustable, isMovable){
+        //Defaults which can be changed by the subclass class.
+        this.color = color;
+        this.seatHeight = seatHeight;
+        this.recliningAngle = recliningAngle;
+        this.backSupport = true;
+        this.headSupport = false;
+        this.padding = "3 inch";
+        this.armRests = true;
+        this.seatSize = "16 inch";
+        this.isHeightAdjustable = false;
+        this.isMovable = false;
+        this.type = "Chair";
+    }
+    
+    adjustableHeight() {};
+    adjustAngle(){};
+    moveChair(){};    
+}
+
+const newChair = new Chair();
