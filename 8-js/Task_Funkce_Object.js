b90af55ee6cf75e7681dcd9ea8955360cc03c535
Jan Peterek <peterekja@seznam.cz>
2023-03-16 14:13:47 +0100

    no message

--- 8-js/Task_Funkce_Object.js
+++ 8-js/Task_Funkce_Object.js
@@ -261,96 +261,131 @@ console.log("(-----------------InternetBanking------------------)")
 
 
 class InternetBanking {
+    exchangeRate = 26.50 
+    transactions = [];
+   
+    accountBalance = {
+        CZK: 0
+    };     
+    constructor() {
 
-    ratio = 26.50
-    accounBalance = null;
-    all_payment = [];
-    debit_payment = [];
-    credit_payment = [];
-    comparison = null;
-    
-    
-    constructor(count,currency) {
-
-        //this.timestamp = timestamp;
-        this.count = count;
-        this.currency = currency;
+    };
 
 
-    }
-
-    toCZK (){
-        
-    }
-
-    creditPayment(count,currency){
-        if(currency == 'CZK'){
-            this.all_payment.push(count);
-            this.credit_payment.push(count);
-        }else if(currency == 'EU'){
-            this.all_payment.push(count*this.ratio);
-            this.credit_payment.push(count*this.ratio);
-        };
-    }
-    debitPayment(count,currency){
-        if(currency == 'CZK'){
-            this.all_payment.push(-count);
-            this.debit_payment.push(-count);
-        }else if(currency == 'EU'){
-            this.all_payment.push(-count*this.ratio);
-            this.debit_payment.push(-count*this.ratio);
-        };
-    }
-
-    countAll(){
-        var suma = 0;
-        for(var i = 0; i < this.all_payment.length; i++){
-            suma += this.all_payment[i] 
+    creditPayment(timestamp, amount, currency) {
+        let value = amount
+        this.transactions.push({
+            ID: this.generateID(),
+            type: "Credit",
+            timestamp,
+            amount,
+            currency
+        })
+        if(currency == "EUR"){
+            value = value * this.exchangeRate
         }
-        this.accounBalance = suma
+        this.accountBalance["CZK"] += value
+
+    }
+    debitPayment(timestamp, amount, currency) {
+        let value = amount
+        this.transactions.push({
+            ID: this.generateID(),
+            type: "Debit",
+            timestamp,
+            amount,
+            currency
+        })
+        if(currency == "EUR"){
+            value = value * this.exchangeRate;
+        }
+        this.accountBalance["CZK"] -= value;
+
 
     }
 
-    // comparison(count,currency,count2,currency2){
-    //     var count = 0
-    //     var count2 = 0
-        
-    //     if(currency == "EU" ){
-    //         count = count* this.ratio
-    //     }
-    //     if(currency2 == "EU" ){
-    //         count2 = count2 * this.ratio
-    //     };
+    generateID(){
+        var uniqueID = Math.floor(Math.random() * Date.now()).toString(16);
+        return uniqueID
+    }
+    compareAmounts(amount, currency, amount2, currency2){
+        if(currency == 'CZK' && currency2 == 'EUR'){
+            amount2 = amount2 * this.exchangeRate;
+        }
+        if(currency == 'EUR' && currency2 == 'CZK'){
+            amount = amount * this.exchangeRate;
+        }
+        return amount == amount2        
 
-    //     if(count === count2){
-    //         return true
-    //     }else{
-    //         return false
-    //     }
-        
+    }
+    printAllTransaction(){
+        console.log("List of all transactions:");
+        console.log(this.transactions);
+    }
+
+    printAllCreditTransaction(){
+        console.log("List of all credit transactions:");
+        this.transactions.filter(transaction => transaction.type === "Credit").forEach(transaction => {
+            console.log(`${transaction.timestamp} - ${transaction.amount} ${transaction.currency}`);
+        });
 
 
+        this.transactions.forEach(function(transaction){             //vybere hodnoty, ale nevrati je
+            if(transaction.type == "Credit"){
+                console.log(`${transaction.timestamp} - ${transaction.amount} ${transaction.currency}`)
+            }
+        })
+      
+    }
+    getCreditTransactions(){ //vrati hodnoty, ale nevytiskne je. Lepsi pro dalsi zpracovani dat
+        return this.transaction.filter(function(transaction){
+            return transaction.type == "Credit"
+        })
+    }
 
+    printCreditTransactions(){
+        console.log(this.getCreditTransactions());
+    }
 
-   // }
+    printAllDebitTransaction(){
+        console.log("List of all debit transactions:");
+        this.transactions.filter(transaction => transaction.type === "Debit").forEach(transaction => {
+          console.log(`${transaction.timestamp} - ${transaction.amount} ${transaction.currency}`);
+        });
+      
+    }
 
+    printAccountBalance(){
+        console.log("Account balance:");
+        console.log(this.accountBalance);
+
+    }
     
 
-    
+
 }
 
 
 
+let banking = new InternetBanking();
 
-var result = new InternetBanking()
-
-result.creditPayment(20,"CZK")
-result.creditPayment(20,"EU")
-result.debitPayment(15,"CZK")
-result.debitPayment(20,"EU")
-
-//result.comparison(20,"EU",20,"EU")
+banking.creditPayment("22.12.2005", 10, "EUR");
+banking.creditPayment('5.5.2555', 10, "CZK");
+banking.debitPayment('4.4.2045', 10, "EUR");
+banking.debitPayment('1.1.2058', 5, "CZK");
 
 
-result.countAll()
-console.log(result)
+banking.compareAmounts(10,"EUR", 10, "EUR");
+//console.log(banking.compareAmounts(10,"EUR", 10, "EUR"))
+
+banking.printAllTransaction()
+banking.printAccountBalance()
+//banking.printAllCreditTransaction()
+//banking.printAllDebitTransaction()
+
+
+
+
+//console.log(banking);
+
+
