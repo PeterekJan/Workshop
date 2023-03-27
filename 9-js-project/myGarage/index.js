ed71dda3be65953497292637702c135df02ad52b
Jan Peterek <peterekja@seznam.cz>
2023-03-27 13:36:59 +0200

    no message

---
+++ 9-js-project/myGarage/index.js
@@ -0,0 +1,6 @@
+import { Car } from "./myLib/Car.js"
+
+
+let car = new Car(1, "Mazda", "6", 2020, 1000000 );
+
+console.log(car.output);
---
+++ 9-js-project/myGarage/myLib/Car.js
@@ -0,0 +1,32 @@
+export class Car {
+
+    id;
+
+    brand;
+    model;
+    year;
+
+    price;
+
+
+
+	constructor(id, brand, model, year, price) {
+	
+        this.id = id;
+
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+
+        this.price = price;
+    
+       
+    }
+    get output() {
+        return "#" + this.id + " " + this.brand + " " + this.model + " (" + this.year + ") - price: " + this.price 
+    }
+
+    
+
+
+}
