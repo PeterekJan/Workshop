3761153c24e9ecd71e806bbcf765f204cb87c330
Jan Peterek <peterekja@seznam.cz>
2023-03-28 11:30:50 +0200

    no message

--- 9-js-project/InternetBanking/index.js
+++ 9-js-project/InternetBanking/index.js
@@ -3,4 +3,19 @@ import { Transaction } from "./myLib/Transaction.js";
 import { Helpers } from "./myLib/Helpers.js";
 import { Owner } from "../myGarage/myLib/Owner.js";
 
+import express from "express";
+import bodyParser from "body-parser";
 
+const app = express();  //ulozena konfigurace serveru
+app.use(bodyParser.json()); //automaticke parsovani do json v restovém rozhraní
+
+
+
+
+
+
+
+
+app.listen(3000, function(){  //naslouchaní na portu 3000, vypsani do konzole pro usnadněni
+    console.log("Example app server listening on localhost port 3000. Try http://localhost:3000");
+});
