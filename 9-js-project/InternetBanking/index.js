4e1e3a84aa11af034811bdf02616321da22ff20e
Jan Peterek <peterekja@seznam.cz>
2023-03-28 10:57:59 +0200

    no message

--- 9-js-project/InternetBanking/index.js
+++ 9-js-project/InternetBanking/index.js
@@ -1,2 +1,6 @@
 import { InternetBanking } from "./myLib/InternetBanking.js";
 import { Transaction } from "./myLib/Transaction.js";
+import { Helpers } from "./myLib/Helpers.js";
+import { Owner } from "../myGarage/myLib/Owner.js";
+
+
